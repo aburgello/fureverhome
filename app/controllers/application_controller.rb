422c09fb9837deab51457677ee30ff00f2dde7e1
Ted Lindsay <tedlindsay@outlook.com>
2024-10-08 15:19:38 +0100

    added all the sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    pets_path # or any other path you want to redirect to
+  end
 end
