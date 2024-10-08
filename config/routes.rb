2f9cbbfd0176cfb480dd57cb87741a5d77b9e313
Ted Lindsay <tedlindsay@outlook.com>
2024-10-08 12:45:22 +0100

    added a landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,6 +9,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  root to: "pets#index"
+
+  get "pets" => "pets#index", as: :pets
+
   # Defines the root path route ("/")
-  # root "posts#index"
 end
