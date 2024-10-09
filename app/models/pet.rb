c820d8b140b3f8d640b0fcf241e0b9b92409cd61
Ted Lindsay <tedlindsay@outlook.com>
2024-10-09 19:03:29 +0100

    update

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,7 +5,7 @@ class Pet < ApplicationRecord
     if image.attached?
       Rails.application.routes.url_helpers.rails_blob_url(image, only_path: true)
     else
-      image_url
+      image_url.presence || 'default_image.png'
     end
   end
 end
