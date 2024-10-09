3a1c844bbd5a98c2e6141a585161c492612a73c7
Antonio Burgello <burgello.antonio@gmail.com>
2024-10-09 11:58:21 +0100

    Added references in pet and user models, added controllers methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :pets, dependent: :destroy
+
+  def owner?
+    role == 'owner'
+  end
+
+  def adopter?
+    role == 'adopter'
+  end
 end
