466eca164fa4591350beedc5bdbce5b820953b9f
Ted Lindsay <tedlindsay@outlook.com>
2024-10-10 19:55:14 +0100

    update

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -1,6 +1,7 @@
 class Adoption < ApplicationRecord
   belongs_to :pet
   belongs_to :user
+  has_many :messages, dependent: :destroy
 
   validates :adopter_name, presence: true
   validates :adopter_email, presence: true
