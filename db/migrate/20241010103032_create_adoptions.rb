6e85280bb039baeecb56b173521d40cf571e5a67
Ted Lindsay <tedlindsay@outlook.com>
2024-10-10 12:32:40 +0100

    update

--- db/migrate/20241010103032_create_adoptions.rb
+++ db/migrate/20241010103032_create_adoptions.rb
@@ -1,10 +1,3 @@
 class CreateAdoptions < ActiveRecord::Migration[7.2]
-  def change
-    create_table :adoptions do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :pet, null: false, foreign_key: true
 
-      t.timestamps
-    end
-  end
 end
